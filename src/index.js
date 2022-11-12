774de7ef45dc0555dc19fcc4e86f2006e4d8649c
ilya.kornienko <ilya.kornienko@chulakov.ru>
2022-11-13 02:19:24 +0300

    make header

--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
 import './styles/main.scss';
+
+// For toggling and finding number of children and other stuff is done here!
+
+const navigation = document.querySelector('.header__nav');
+const menu = document.querySelector('.header__menu');
+const phone = document.querySelector('.header__phone');
+
+navigation.addEventListener('click', () => {
+  // The navigation.children.length means the following :-
+  // The children inside a parent are basically an array of elements; So, here I'm finding the length of the array aka how many children are inside the nav bar.
+  //   Yup That's all.
+  // navigation.style.setProperty('--childenNumber', navigation.children.length);
+
+  //    Casually Toggling Classes to make them animate on click
+  //   Regular stuff ;)
+  navigation.classList.toggle('header__menu-burger--active');
+  menu.classList.toggle('header__menu--active');
+  phone.classList.toggle('header__phone--active');
+});
