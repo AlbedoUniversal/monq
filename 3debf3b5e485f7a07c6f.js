951a2046f60ed65b87507a7e28a5bc4db792c56b
Artur Procenko <artur.procenko@chulakov.ru>
2022-11-19 18:24:08 +0300

    new vers

---
+++ 3debf3b5e485f7a07c6f.js
@@ -0,0 +1,48 @@
+import './styles/main.scss';
+import { phoneInput } from './phoneinput.js';
+import { postForm } from './postForm.js';
+
+// For toggling and finding number of children and other stuff is done here!
+
+const navigation = document.querySelector('.header__nav');
+const menu = document.querySelector('.header__menu');
+const menuBurger = document.querySelector('.header__menu-burger');
+const phone = document.querySelector('.header__phone');
+const openModal = document.querySelector('.umbrellaMonitoring__more');
+const modal = document.querySelector('.modal');
+const closeModal = modal.querySelector('.modal__close-button');
+
+const openResultModal = document.querySelector('.result__button');
+const resultModal = document.querySelector('.resultModal');
+const closeResultModal = resultModal.querySelector('.resultModal__close-button');
+
+phoneInput();
+postForm();
+
+menuBurger.addEventListener('click', () => {
+  navigation.classList.toggle('header__menu-burger--active');
+  menu.classList.toggle('header__menu--active');
+  phone.classList.toggle('header__phone--active');
+});
+
+openModal.addEventListener('click', () => {
+  document.body.classList.add('modal__non-scroll');
+  modal.classList.add('modal--active');
+});
+
+closeModal.addEventListener('click', () => {
+  document.body.classList.remove('modal__non-scroll');
+  modal.classList.remove('modal--active');
+});
+
+openResultModal.addEventListener('click', () => {
+  document.body.classList.add('resultModal__non-scroll');
+  resultModal.classList.add('resultModal--active');
+});
+
+closeResultModal.addEventListener('click', () => {
+  document.body.classList.remove('resultModal__non-scroll');
+  resultModal.classList.remove('resultModal--active');
+});
+
+
